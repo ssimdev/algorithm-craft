1c804c3b656b80c7bd3f6d176ae85ef2849aae24
ssimdev <ssimdev@gmail.com>
2020-10-28 10:53:26 +0100

    Updates

--- precache-manifest.832cc6f550e208857965685a28e728c6.js
+++ precache-manifest.832cc6f550e208857965685a28e728c6.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2aee3faf7d8eed4abfc4608d619e9b9f",
+    "revision": "d369984dba6f273127acaab079908ea6",
     "url": "/index.html"
   },
   {
-    "revision": "6feb354921189d9f2aaf",
-    "url": "/static/css/main.80d68317.chunk.css"
+    "revision": "d98d7f6846d3d2cc9316",
+    "url": "/static/css/main.8a67c410.chunk.css"
   },
   {
     "revision": "89e3ae19982c32a04618",
@@ -16,11 +16,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.df867b60.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6feb354921189d9f2aaf",
-    "url": "/static/js/main.d6d5558b.chunk.js"
+    "revision": "d98d7f6846d3d2cc9316",
+    "url": "/static/js/main.37ba9e63.chunk.js"
   },
   {
     "revision": "976430da955fd4d227cc",
     "url": "/static/js/runtime-main.b58e4cbb.js"
+  },
+  {
+    "revision": "d3929e008f6c6d396b128265ef066b44",
+    "url": "/static/media/nodejs.d3929e00.png"
   }
 ]);
